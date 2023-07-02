9c22bfe4816139467d478d1c2ccd51d11403524d
Muhammad Saad <saad345khan@gmail.com>
2023-07-02 00:18:48 -0700

    updated

--- Object detection.py
+++ Object detection.py
@@ -26,7 +26,7 @@ detection_model = model_builder.build(model_config=configs['model'], is_training
 # Restore checkpoint
 ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
 ckpt.restore(os.path.join(CHECKPOINT_PATH, 'ckpt-21')).expect_partial()
-
+#tf function
 @tf.function
 def detect_fn(image):
     image, shapes = detection_model.preprocess(image)
